524f12573b3d0864d4e11e62852c6a294b80e0e6
Pran k k <prankrishnakk340@gmail.com>
2025-03-26 18:42:36 +0530

    linq

--- Program.cs
+++ Program.cs
@@ -4,76 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace advance_oops
+namespace ConsoleApp1
 {
-
-
-    interface Shape
-    {
-        double GetArea();
-    }
-
-
-    class Circle : Shape
-    {
-        private double radius;
-
-        public Circle(double radius)
-        {
-            this.radius = radius;
-        }
-
-        public double GetArea()
-        {
-            return Math.PI * radius * radius;
-        }
-    }
-
-
-    class Rectangle : Shape
-    {
-        private double length, width;
-
-        public Rectangle(double length, double width)
-        {
-            this.length = length;
-            this.width = width;
-        }
-
-        public double GetArea()
-        {
-            return length * width;
-        }
-    }
-
-    class Square : Shape
-    {
-        private double side;
-
-        public Square(double side)
-        {
-            this.side = side;
-        }
-
-        public double GetArea()
-        {
-            return side * side;
-        }
-    }
-
-
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Shape circle = new Circle(5);
-            Shape rectangle = new Rectangle(4, 6);
-            Shape square = new Square(3);
+          
+            int [] num = { 2, 2, 1 };
+            Console.WriteLine(num.Skip(2).First());
 
-            Console.WriteLine("Area of Circle: " + circle.GetArea());
-            Console.WriteLine("Area of Rectangle: " + rectangle.GetArea());
-            Console.WriteLine("Area of Square: " + square.GetArea());
+            //string bracket = "()";
+
+            //var symbol = bracket.SingleOrDefault(() == ());
+            //if (() != ())
+            //{
+            //    Console.WriteLine("false");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("nothing");
+            //}
+
+
+
+
         }
     }
-
 }
